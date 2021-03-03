d147349b5c500488d7048df665474d6b3a96b8f2
Dharmveer Baloda <dharmveer.baloda@erosnow.com>
2021-03-03 11:58:20 +0530

    changing module name

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="baloda", # Replace with your own username
+    name="subconverter", # Replace with your own username
     version="0.0.1",
     author="Dharmveer Baloda",
     author_email="dharmvrbaloda836@gmail.com",
